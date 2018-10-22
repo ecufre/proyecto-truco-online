f1f2141958978cf086ed0901e20c1ae6cb90fe45
Eze <ecufre96@gmail.com>
2018-10-21 22:21:31 -0300

    Persistencia Partida (PartidaDAO)

--- TP Truco Online/src/dao/PartidaDAO.java
+++ TP Truco Online/src/dao/PartidaDAO.java
@@ -1,7 +1,21 @@
 package dao;
 
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import controladores.HibernateUtil;
+import entities.BazaEntity;
+import entities.CantoEntity;
+import entities.CartaEntity;
+import entities.JuegoEntity;
 import entities.JugadorEntity;
 import entities.PartidaEntity;
+import excepciones.ComunicacionException;
+import negocio.Baza;
+import negocio.Carta;
+import negocio.Juego;
 import negocio.Jugador;
 import negocio.Partida;
 
@@ -19,28 +33,102 @@ private PartidaDAO() {}
 		return instancia;
 	}
 	
-	public PartidaEntity getPartidaById(int id) {
-		return null; //TODO
+	public Partida getPartidaById(int id) throws ComunicacionException {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		PartidaEntity pe = (PartidaEntity) session.createQuery("from PartidaEntity where id = ?")
+					.setParameter(0, id)
+					.uniqueResult();
+		if(pe != null){
+			return PartidaDAO.getInstancia().toNegocio(pe);
+		}
+		else 
+			throw new ComunicacionException("La Partida solicitado no existe");
 	}
 	
 	
 	public Integer crear(Partida p) {
 		
-		return null; //TODO
+		PartidaEntity pe = new PartidaEntity(p.isEsAbierta(), p.getEstado(), p.getGanador());
+		ArrayList<JuegoEntity> juegos = new ArrayList<JuegoEntity>();
+		for (Juego j : p.getJuegos()) {
+			JuegoEntity je = new JuegoEntity();
+			je.setId(j.getId());
+		}
+		pe.setJuegos(juegos);
+		ArrayList<JugadorEntity> jugadores = new ArrayList<JugadorEntity>();
+		for (Jugador j : p.getJugadores()) {
+			JugadorEntity je = new JugadorEntity();
+			je.setApodo(j.getApodo());
+		}
+		pe.setJugadores(jugadores);
+		ArrayList<JugadorEntity> jugadoresListos = new ArrayList<JugadorEntity>();
+		for (Jugador j : p.getJugadoresListos()) {
+			JugadorEntity je = new JugadorEntity();
+			je.setApodo(j.getApodo());
+		}
+		pe.setJugadoresListos(jugadoresListos);
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		Integer numero = (Integer)session.save(pe);
+		session.getTransaction().commit();
+		session.close();
+		return numero;
 	}
 	
 	public void grabar(Partida p) {
 		
-		//TODO
+		PartidaEntity pe = new PartidaEntity(p.getId(), p.isEsAbierta(), p.getEstado(), p.getGanador());
+		ArrayList<JuegoEntity> juegos = new ArrayList<JuegoEntity>();
+		for (Juego j : p.getJuegos()) {
+			JuegoEntity je = new JuegoEntity();
+			je.setId(j.getId());
+		}
+		pe.setJuegos(juegos);
+		ArrayList<JugadorEntity> jugadores = new ArrayList<JugadorEntity>();
+		for (Jugador j : p.getJugadores()) {
+			JugadorEntity je = new JugadorEntity();
+			je.setApodo(j.getApodo());
+		}
+		pe.setJugadores(jugadores);
+		ArrayList<JugadorEntity> jugadoresListos = new ArrayList<JugadorEntity>();
+		for (Jugador j : p.getJugadoresListos()) {
+			JugadorEntity je = new JugadorEntity();
+			je.setApodo(j.getApodo());
+		}
+		pe.setJugadoresListos(jugadoresListos);
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.saveOrUpdate(pe);
+		session.getTransaction().commit();
+		session.close();
 	}
 	
-	public void actualizar(Partida p) {
-
-		//TODO
-	}
 	
-	public Partida toNegocio(PartidaEntity pe) {
-		return null; //TODO
+	
+	public Partida toNegocio(PartidaEntity pe) throws ComunicacionException {
+		Partida p = new Partida(pe.getEsAbierta());
+		p.setId(pe.getId());
+		p.setEstado(pe.getEstado());
+		p.setGanador(pe.getGanador());
+		ArrayList<Juego> juegos = new ArrayList<Juego>();
+		for (JuegoEntity je : pe.getJuegos()) {
+			juegos.add(JuegoDAO.getInstancia().toNegocio(je));
+		}
+		p.setJuegos(juegos);
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		for (JugadorEntity je : pe.getJugadores()) {
+			jugadores.add(JugadorDAO.getInstancia().toNegocio(je));
+		}
+		p.setJugadores(jugadores);
+		ArrayList<Jugador> jugadoresListos = new ArrayList<Jugador>();
+		for (JugadorEntity je : pe.getJugadores()) {
+			jugadoresListos.add(JugadorDAO.getInstancia().toNegocio(je));
+		}
+		p.setJugadoresListos(jugadoresListos);
+		return p;
 	}
 	
 	public PartidaEntity toEntity(Partida p) {
