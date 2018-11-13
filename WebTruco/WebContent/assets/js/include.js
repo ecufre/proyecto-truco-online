0d6489becb7d95e7ad123e1104b914228b5b757e
Fernando Basteiro <fernando@basteiro.com.ar>
2018-11-13 00:25:24 -0300

    Finalizado script para el listado de partidas

--- WebTruco/WebContent/assets/js/include.js
+++ WebTruco/WebContent/assets/js/include.js
@@ -52,23 +52,35 @@ function actualizarPartidas() {
 			var listaPartidas = JSON.parse(this.responseText);
 			for (i = 0; i < listaPartidas.Pendientes.length; i++) {
 				var idPartida = listaPartidas.Pendientes[i].idPartida;
-				var hiddenInput = document.getElementById("partida_p_" + idPartida);
+				var hiddenInput =document.getElementById("partida_p_" + idPartida);
 				if (hiddenInput == null) {
 					$("#tablaPendientes").append("<tr align='center' id='partida_p_" + idPartida + "'><td>" + idPartida + "</td><input type='hidden' id='update_" + idPartida + "' value=" + listaPartidas.Pendientes[i].lastUpdate + "></tr>");
 				}
 			}
 			for (i = 0; i < listaPartidas.EnCurso.length; i++) {
 				var idPartida = listaPartidas.EnCurso[i].idPartida;
-				var hiddenInputP = document.getElementById("partida_p_" + idPartida);
-				if (hiddenInputP != null) {
-					var jqueryrow = $("#partida_p_" + idPartida);
-					jqueryrow.remove();
+				var partidaPendienteRow = document.getElementById("partida_p_" + idPartida);
+				if (partidaPendienteRow != null) {
+					var jqpartidaPendienteRow = $("#partida_p_" + idPartida);
+					partidaPendienteRow.remove();
 				}
-				var hiddenInputC = document.getElementById("partida_" + idPartida);
-				if (hiddenInputC == null) {
-					$("#tablaEnCurso").append("<tr align='center' id='partida_" + idPartida + "' onclick=\"loadDiv('principal', 'Partidas?action=mostrarPartida', 'partidaId=" + idPartida + "')\"><td>" + idPartida + "</td><input type='hidden' id='update_" + idPartida + "' value=" + listaPartidas.Pendientes[i].lastUpdate + "></tr>");
+				var partidaEnCursoRow = document.getElementById("partida_" + idPartida);
+				if (partidaEnCursoRow == null) {
+					$("#tablaEnCurso").append("<tr align='center' id='partida_" + idPartida + "' onclick=\"loadDiv('principal', 'Partidas?action=mostrarPartida', 'partidaId=" + idPartida + "'); this.className=''\"><td>" + idPartida + "</td><input type='hidden' id='update_" + idPartida + "' value=0></tr>");
 				}
-				
+				var jqPartidaEnCursoRow = $("#partida_" + idPartida);
+				var hiddenInputC = $("#update_" + idPartida);
+				if (listaPartidas.EnCurso[i].lastUpdate > hiddenInputC.val()) {
+					jqPartidaEnCursoRow.addClass('warning');
+					hiddenInputC.val(listaPartidas.EnCurso[i].lastUpdate);
+				}
+
+				if (listaPartidas.EnCurso[i].miTurno) {
+					jqPartidaEnCursoRow.addClass('danger').removeClass('warning');
+				}
+				else {
+					jqPartidaEnCursoRow.removeClass('danger');
+				}
 			}
 		}
 		else if (this.readyState == 4 && this.status == 599) {document.getElementById("mensajes").innerHTML = this.responseText;}
